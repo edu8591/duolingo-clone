1fbcf3942cf6951658b1312fc65c160cded0a0cb
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-27 13:13:14 -0500

    implement header and basic hero section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,21 @@
+import { ClerkLoading } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-green-500 font-bold">Hello Lingo!</p>
+    <div className="max-w-[988px] mx-auto flex flex-col flex-1 w-full  lg:flex-row items-center justify-center gap-2">
+      <div className="relative w-[240px] h-[240px] lg:h-[424px] lg:w-[424px] mb-8 lg:mb-0">
+        <Image src="/images/hero.svg" fill alt="hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice and master new languages with Lingo.
+        </h1>
+        <ClerkLoading>
+          <Loader className="animate-spin h-5 w-5 text-muted-foreground" />
+        </ClerkLoading>
+      </div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,45 @@
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import { Button } from "./ui";
+
 export const Header = () => {
   return (
-    <div className="h-20 w-full border-b-2 border-slate-200 px4">Header</div>
+    <header className="h-20 w-full border-b-2 border-slate-200 px4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/images/mascot.png" height={40} width={40} alt="mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              forceRedirectUrl="/learn"
+              signUpForceRedirectUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
+    </header>
   );
 };
