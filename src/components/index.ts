ce5a9349677b3c59c8f89340bb3221c93246ac20
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-27 15:32:45 -0500

    create Moobileheader and MobileSidebar install the sheet component from shadcn

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
+import { MobileHeader } from "./MobileHeader";
+
 export * from "./ui";
 export * from "./Header";
 export * from "./Footer";
 export * from "./HeroSection";
+export * from "./Sidebar";
+export * from "./MobileHeader";
