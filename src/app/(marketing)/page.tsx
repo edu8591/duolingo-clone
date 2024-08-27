d4a57b06184315d15c6e00d6d0067027e64d464d
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-27 14:27:27 -0500

    add flags images and use the in the footer

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,21 +1,20 @@
-import { ClerkLoading } from "@clerk/nextjs";
+import { Button, HeroSection } from "@/components";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="max-w-[988px] mx-auto flex flex-col flex-1 w-full  lg:flex-row items-center justify-center gap-2">
-      <div className="relative w-[240px] h-[240px] lg:h-[424px] lg:w-[424px] mb-8 lg:mb-0">
-        <Image src="/images/hero.svg" fill alt="hero" />
-      </div>
-      <div className="flex flex-col items-center gap-y-8">
-        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
-          Learn, practice and master new languages with Lingo.
-        </h1>
-        <ClerkLoading>
-          <Loader className="animate-spin h-5 w-5 text-muted-foreground" />
-        </ClerkLoading>
-      </div>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,61 @@
+import Image from "next/image";
+import { Button } from "./ui";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/images/flags/DE.svg"
+            alt="German Flag"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          German
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/images/flags/ES.svg"
+            alt="Spanish Flag"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/images/flags/FR.svg"
+            alt="France Flag"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/images/flags/JP.svg"
+            alt="Japan Flag"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/images/flags/RU.svg"
+            alt="Russia Flag"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Russian
+        </Button>
+      </div>
     </footer>
   );
 };
