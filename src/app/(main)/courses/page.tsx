245c18dedd238e8b1cfba80de9df6c7736f983cd
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 14:01:17 -0500

    implement basic CoursesPage view

---
+++ src/app/(main)/courses/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function CoursesPage() {
+  return (
+    <div className="h-full maw-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">Languages Courses</h1>
+    </div>
+  );
+}
