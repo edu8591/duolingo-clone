8ed2bfb8896e93400b82bec318ce4869629026ec
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 00:16:20 -0500

    removes bg-red and adds  a max-width and automatic margins

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,7 +13,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
       <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="bg-red-500 h-full">{children}</div>
+        <div className="max-w-[1056px] mx-auto h-full">{children}</div>
       </main>
     </>
   );
