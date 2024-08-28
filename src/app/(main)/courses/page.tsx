b08b2734c46e9dae30e88bb9cb186f7a739dd7e7
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 14:26:17 -0500

    implement List component in the courses page

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -1,9 +1,12 @@
+import { getCourses } from "@/db";
+import { List } from "./List";
 
-
-export default function CoursesPage() {
+export default async function CoursesPage() {
+  const data = await getCourses();
   return (
     <div className="h-full maw-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Languages Courses</h1>
+      <List courses={data} activeCourseId={1} />
     </div>
   );
 }
