490ff9c9d625fdf749da0737bfa2031883258602
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-27 23:27:45 -0500

    extracts the logo and name into the brand component

---
+++ src/components/Brand.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const Brand = () => {
+  return (
+    <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+      <Image src="/images/mascot.png" height={40} width={40} alt="mascot" />
+      <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+        Lingo
+      </h1>
+    </div>
+  );
+};
