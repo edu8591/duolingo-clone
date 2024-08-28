2bbae61db89f5b1171ee38a88cc8cd1c8dcdea22
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 13:40:00 -0500

    setup drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
