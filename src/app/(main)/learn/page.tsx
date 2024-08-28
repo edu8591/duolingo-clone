9244ab66655bf35ded8d4d5044f0504956e5f9a0
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 00:18:16 -0500

    update the learn page to use the newly implemented components

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,3 +1,21 @@
+import { FeedWrapper, StickyWrapper } from "@/components";
+import { Header } from "./Header";
+import { UserProgress } from "@/components/UserProgress";
+
 export default function LearnPage() {
-  return <div> learn page</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/images/flags/ES.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="spanish" />
+      </FeedWrapper>
+    </div>
+  );
 }
