2442e4a23aab950df7a6397c30acafaa2f4e573d
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 19:08:25 -0500

    fetch data onthe courses page

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -1,12 +1,16 @@
-import { getCourses } from "@/db";
+import { getCourses, getUserProgress } from "@/db";
 import { List } from "./List";
 
 export default async function CoursesPage() {
-  const data = await getCourses();
+  const [data, userProgress] = await Promise.all([
+    getCourses(),
+    getUserProgress(),
+  ]);
+
   return (
     <div className="h-full maw-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Languages Courses</h1>
-      <List courses={data} activeCourseId={1} />
+      <List courses={data} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 }
