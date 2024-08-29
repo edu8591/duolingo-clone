f70c984e52ede4199d699e596082fdcb467e7403
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 23:41:29 -0500

    removes hardcoded data to use fetch data

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -6,22 +6,22 @@ import { redirect } from "next/navigation";
 
 export default async function LearnPage() {
   const [userProgress] = await Promise.all([getUserProgress()]);
-
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/images/flags/ES.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
