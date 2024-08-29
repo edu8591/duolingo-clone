75f3f68dab3a418461f70838ba7a00ed453a23e1
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-28 19:08:44 -0500

    fetch data on the LearnPage

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,8 +1,15 @@
 import { FeedWrapper, StickyWrapper } from "@/components";
 import { Header } from "./Header";
 import { UserProgress } from "@/components/UserProgress";
+import { getUserProgress } from "@/db";
+import { redirect } from "next/navigation";
 
-export default function LearnPage() {
+export default async function LearnPage() {
+  const [userProgress] = await Promise.all([getUserProgress()]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
