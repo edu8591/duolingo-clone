29e18b816479e914e5f3e6132b5b57a6d9d118d8
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-29 09:11:11 -0500

    update seed to create a units, lessons and its challenges

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,11 @@ const main = async () => {
     console.log("Seeding DB");
     await db.delete(schema.userProgress);
     await db.delete(schema.courses);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       { title: "Spanish", imageSrc: "/images/flags/ES.svg" },
@@ -21,6 +26,30 @@ const main = async () => {
       { title: "Japanese", imageSrc: "/images/flags/JP.svg" },
       { title: "Russian", imageSrc: "/images/flags/RU.svg" },
       { title: "English", imageSrc: "/images/flags/US.svg" },
+    ]);
+
+    const courses = await db.query.courses.findMany();
+
+    await db.insert(schema.units).values({
+      id: 1,
+      courseId: courses[0].id,
+      title: "Unit 1",
+      description: `Learn the basics of ${courses[0].title}`,
+      order: 1,
+    });
+
+    const units = await db.query.units.findMany();
+    await db.insert(schema.lessons).values([
+      {
+        title: "Nouns",
+        unitId: units[0].id,
+        order: 1,
+      },
+      {
+        title: "Verbs",
+        unitId: units[0].id,
+        order: 2,
+      },
     ]);
   } catch (error) {
     console.error(error);
