37d32d3143b82a7fc48eaf7d60c7dcdf3fa71f29
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-08-31 14:17:34 -0500

    creates the Unit component

---
+++ src/app/(main)/learn/Unit.tsx
@@ -0,0 +1,51 @@
+import { SelectLessons, SelectUnits } from "@/db";
+import { UnitBanner } from "./UnitBanner";
+import { LessonButton } from "./LessonButton";
+
+interface UnitProps {
+  id: number;
+  order: number;
+  title: string;
+  description: string;
+  lessons: (SelectLessons & { completed: boolean })[];
+  activeLesson:
+    | (SelectLessons & {
+        units: SelectUnits;
+      })
+    | undefined;
+  activeLessonPercentage: number;
+}
+
+export const Unit = ({
+  id,
+  order,
+  title,
+  description,
+  lessons,
+  activeLesson,
+  activeLessonPercentage,
+}: UnitProps) => {
+  return (
+    <>
+      <UnitBanner title={title} description={description} />
+      <div className="flex items-center flex-col relative">
+        {lessons.map((lesson, i) => {
+          const isCurrent = lesson.id === activeLesson?.id;
+          const isLocked = !lesson.completed && !isCurrent;
+
+          return (
+            <LessonButton
+              key={lesson.id}
+              id={lesson.id}
+              index={i}
+              locked={isLocked}
+              totalCount={lessons.length - 1}
+              current={isCurrent}
+              percentage={activeLessonPercentage}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
