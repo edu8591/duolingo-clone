ef275965cc285dad13e0b9e113fb58f691470208
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-03 16:33:12 -0500

    redirect the user when not signed in

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -1,12 +1,18 @@
 import { getCourses, getUserProgress } from "@/db";
 import { List } from "./List";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function CoursesPage() {
   const [data, userProgress] = await Promise.all([
     getCourses(),
     getUserProgress(),
   ]);
+  const { userId } = auth();
 
+  if (!userId) {
+    redirect("/");
+  }
   return (
     <div className="h-full maw-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Languages Courses</h1>
