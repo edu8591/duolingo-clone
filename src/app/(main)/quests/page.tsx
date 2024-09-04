5a025b4c370f09a378c15526d40eaf1226d8cdcb
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-04 12:03:03 -0500

    create dummy quests and render them

--- src/app/(main)/quests/page.tsx
+++ src/app/(main)/quests/page.tsx
@@ -1,12 +1,20 @@
 import {
   FeedWrapper,
-  Separator,
+  Progress,
   StickyWrapper,
   UserProgress,
 } from "@/components";
-import { getTopTenUsers, getUserProgress, getUserSubscription } from "@/db";
+import { getUserProgress, getUserSubscription } from "@/db";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+
+const quests = [
+  { title: "Earn 20 xp", value: 20 },
+  { title: "Earn 50 xp", value: 50 },
+  { title: "Earn 100 xp", value: 100 },
+  { title: "Earn 500 xp", value: 500 },
+  { title: "Earn 1,000 xp", value: 1000 },
+];
 
 export default async function QuestsPage() {
   const [userProgress, userSubscription] = await Promise.all([
@@ -30,19 +38,39 @@ export default async function QuestsPage() {
       </StickyWrapper>
       <FeedWrapper>
         <div className="flex flex-col items-center w-full">
-          <Image
-            src="/images/leaderboard.svg"
-            height={90}
-            width={90}
-            alt="Leaderboard"
-          />
+          <Image src="/images/quest.svg" height={90} width={90} alt="quests" />
           <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
-            Leaderboard
+            Quests
           </h1>
           <p className="text-muted-foreground text-center text-lg mb-6">
-            See where you stand among other learners in the community.
+            Complete quests by earning points.
           </p>
-          <Separator className="mb-5 h-0.5 rouded-full" />
+          {/* //todo add quests  */}
+          <ul className="w-full">
+            {quests.map((quest) => {
+              const progress = (userProgress.points / quest.value) * 100;
+
+              return (
+                <div
+                  className="flex items-center w-full p-4 gap-x-4 border-t-2"
+                  key={quest.title}
+                >
+                  <Image
+                    src="/images/points.svg"
+                    width={60}
+                    height={60}
+                    alt="thunder"
+                  />
+                  <div className="flex flex-col gap-y-2 w-full">
+                    <p className="text-neutral-700 text-xl font-bold">
+                      {quest.title}
+                    </p>
+                    <Progress value={progress} className="h-3" />
+                  </div>
+                </div>
+              );
+            })}
+          </ul>
         </div>
       </FeedWrapper>
     </div>
