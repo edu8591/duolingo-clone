374079ba4897ba98b87dab1b2cb06cc1bc5a370e
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-09-04 11:39:21 -0500

    get and render top ten users

--- src/app/(main)/leaderboard/page.tsx
+++ src/app/(main)/leaderboard/page.tsx
@@ -1,12 +1,20 @@
-import { FeedWrapper, StickyWrapper, UserProgress } from "@/components";
-import { getUserProgress, getUserSubscription } from "@/db";
+import {
+  Avatar,
+  AvatarImage,
+  FeedWrapper,
+  Separator,
+  StickyWrapper,
+  UserProgress,
+} from "@/components";
+import { getTopTenUsers, getUserProgress, getUserSubscription } from "@/db";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function LeaderboardPage() {
-  const [userProgress, userSubscription] = await Promise.all([
+  const [userProgress, userSubscription, topTenUsers] = await Promise.all([
     getUserProgress(),
     getUserSubscription(),
+    getTopTenUsers(),
   ]);
   const isPro = !!userSubscription?.isActive;
   if (!userProgress || !userProgress.activeCourse) {
@@ -37,8 +45,31 @@ export default async function LeaderboardPage() {
           <p className="text-muted-foreground text-center text-lg mb-6">
             See where you stand among other learners in the community.
           </p>
+          <Separator className="mb-5 h-0.5 rouded-full" />
           {
             //todo add UserList
+            topTenUsers.map((userProgress, index) => {
+              return (
+                <div
+                  key={userProgress.userId}
+                  className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
+                >
+                  <p className="font-bold text-lime-700 mr-4"> {index + 1}</p>
+                  <Avatar className="border bg-green-500 h-12 w-12 ml-3 mr-6">
+                    <AvatarImage
+                      className="object-cover"
+                      src={userProgress.userImageSrc}
+                    />
+                  </Avatar>
+                  <p className="font-bold text-neutral-800 flex-1">
+                    {userProgress.userName}
+                  </p>
+                  <p className="text-muted-foreground">
+                    {userProgress.points} XP
+                  </p>
+                </div>
+              );
+            })
           }
         </div>
       </FeedWrapper>
